fec2d2374ac2c6feeaf909c52b1ca908974f9d65
Udith Kavishka <udithmanohara@gmail.com>
2023-05-25 22:02:55 +0530

    1

---
+++ 1. Introduction/Introduction_001/src/I_HelloPeter.java
@@ -0,0 +1,36 @@
+public class I_HelloPeter {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+        System.out.println(3+4);
+
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.print("00");
+        System.out.print(3+4);
+        // 007 - James Bond
+
+        //ex1.10 - Modify Program to greet you:
+        System.out.println("Hello UKhira");
+
+        //ex1.11 - Modify Program to print "Hello" vertically
+        System.out.println("H");
+        System.out.println("e");
+        System.out.println("l");
+        System.out.println("o");
+
+        //ex1.12 Would program work If you change line 4 to below (yes)
+        System.out.println("Hello");
+
+        //ex1.13 What does the following set of statement is print
+        System.out.print("My Lucky Number is ");
+        System.out.println(3+4+5);
+
+        // ex1.14 What does the following statement print
+        System.out.println("Hello");
+        System.out.println(" ");
+        System.out.println("World!");
+    }
+}

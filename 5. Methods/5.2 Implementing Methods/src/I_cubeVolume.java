5d7dddac7a20d13b92123bb249ce9eaf27c2237f
Udith Kavishka <udithmanohara@gmail.com>
2023-06-09 22:48:04 +0530

    getting start with methods

---
+++ 5. Methods/5.2 Implementing Methods/src/I_cubeVolume.java
@@ -0,0 +1,29 @@
+/**
+    This program computes the volumes of two cubes.
+*/
+import java.util.Scanner;
+
+public class I_cubeVolume {
+    
+    public static double cubeVolume(double sideLength){
+        double volume = sideLength * sideLength * sideLength;
+        return volume;
+
+        /**
+         Computes the volume of a cube.
+         @param sideLength the side length of the cube
+         @return the volume
+        */
+    }
+
+    public static void main(String[] args) {
+        double result1 = cubeVolume(2);
+        System.out.println("A cube with side length 2 has volume " + result1);
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter length ");
+        double len = in.nextDouble();
+
+        double result2 = cubeVolume(len);
+        System.out.println("A cube with side length " + len + "has volume " + result2);
+    }
+}

32fc30fc76103dde1abb0883da261f2c219aaf90
Udith Kavishka <udithmanohara@gmail.com>
2023-06-24 10:06:04 +0530

    gettiing started

---
+++ 8. Objects and Classes/7.1 Reading and Writing Text Files/src/I_PriintTotal.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * ! This Program read a file with numbers, and write a numbers to another file
+ * TODOlined up column and there total
+ */
+public class I_PriintTotal {
+    public static void main(String[] args) throws FileNotFoundException{
+        // prompt for input and output file names
+
+        Scanner console = new Scanner(System.in);
+        System.out.println("Input File: ");
+        String inputFileName = console.next();
+        System.out.println("Output File: ");
+        String outputFileName = console.next();
+
+        // Construct the Scanner and printWriter  objects for reading andd writing
+        
+        File inputFile = new File(inputFileName);
+        Scanner in = new Scanner(inputFile);
+        PrintWriter out = new PrintWriter(outputFileName);
+
+        //  Read the input and write the output
+        double total = 0;
+
+        while (in.hasNextDouble()){
+            double value = in.nextDouble();
+            out.printf("%15.2f\n", value);
+            total = total + value;
+        }
+        out.printf("Total: %8.2f\n", total);
+
+        in.close();
+        out.close();
+        console.close();
+    }
+}

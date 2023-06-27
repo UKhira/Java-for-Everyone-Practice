ec6bdc90873b08ca09ec304a49ee0a4f7e2e022b
Udith Kavishka <udithmanohara@gmail.com>
2023-06-28 01:13:25 +0530

    halfway through

--- 8. Objects and Classes/8.6 Constructors/src/CashRegister.java
+++ 8. Objects and Classes/8.6 Constructors/src/CashRegister.java
@@ -4,6 +4,7 @@
 public class CashRegister {
     private int itemCount;
     private double totalPrice;
+    private final double TAX_AMOUNT;
 
     /**
      * Construct a cash register with cleared item count and total
@@ -11,15 +12,17 @@ public class CashRegister {
     public CashRegister(){
         itemCount = 0;
         totalPrice = 0;
+        TAX_AMOUNT = 7.5;
     }
 
     /**
      * Adds an item to this cash register
      * @param price - the price of this item
     */
-    public void addItem(double price){
+    public void addItem(double price, boolean taxable){
         itemCount++;
         totalPrice += price;
+        tax(price,taxable);
     }
 
     /**
@@ -44,6 +47,15 @@ public class CashRegister {
     public void clear(){
         itemCount = 0;
         totalPrice = 0;
+    }
+
+    /**
+     * Add Tax(If available)
+     */
+    public void tax(double price, boolean taxable){
+        if(taxable){
+            totalPrice = price + price * TAX_AMOUNT;
+        }
     }
 
 }
--- 8. Objects and Classes/8.6 Constructors/src/CashRegisterTest.java
+++ 8. Objects and Classes/8.6 Constructors/src/CashRegisterTest.java
@@ -4,13 +4,13 @@
 public class CashRegisterTest {
    public static void main(String[] args) {
     CashRegister register1 = new CashRegister();
-    register1.addItem(1.95);
-    register1.addItem(0.95);
-    register1.addItem(2.50);
+    register1.addItem(1.95, true);
+    register1.addItem(0.95, true);
+    register1.addItem(2.50, false);
 
     System.out.println(register1.getCount());
     System.out.println("Expected: 3");
     System.out.printf("%.2f%n", register1.getTotal());
-    System.out.println("Expected: 5.40");
+    System.out.println("Expected: 5.40");       //Before tax
    }
 }
